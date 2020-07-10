f85b306f468f94b8aab7f50be3bdb8ef8079e980
fuadashraful <fuadashraful16@gmail.com>
2020-07-10 14:26:19 +0600

    suffix array updated

--- suffix_array.cpp
+++ suffix_array.cpp
@@ -1,117 +1,99 @@
 /*
-	this.author = Fuad Ashraful Mehmet
-	Date: 06th July 2020
-	Algo:suffix array of string
+ this->author = Fuad Ashraful Mehmet, CSE-UAP
+ algo: suffix_array from cf
+ added : 10th July 2020
 */
 #include<bits/stdc++.h>
-
+#define forn(i,n) for(int i=0;i<(int)n;++i)
+#define all(o) (o).begin(),(o).end()
+#define dbg(o)  cerr<<"at "<<__LINE__<<" response = "<<o<<endl;
 
 using namespace std;
 
-void radix_sort(vector<pair<pair<int,int> ,int> > &a)
+typedef long long ll;
+int n,m;
+
+void counting_sort(vector<int> & pn ,vector<int> & c)
 {
-	int n=a.size();
-	// soring 2nd part
-	{
-		vector<int>cnt(n),pos(n);
-		for(int i=0;i<n;++i)
-		{
-			cnt[a[i].first.second]++;
-		}
+      vector<int>cnt(n),pos(n);
+      for(int i=0;i<n;++i)
+      {
+            cnt[c[pn[i]]]++;
+      }
+      pos[0]=0;
+      for(int i=1;i<n;++i)
+      {
+            pos[i]=pos[i-1]+cnt[i-1];
+      }
 
-		pos[0]=0;
-		for(int i=1;i<n;++i)
-		{
-			pos[i]=pos[i-1]+cnt[i-1];
-		}
-		vector<pair<pair<int,int>,int> >new_a(n);
-		for(int i=0;i<n;++i)
-		{
-			int p=a[i].first.second;
-			new_a[pos[p]]=a[i];
-			pos[p]++;
-		}
-		a=new_a;
-	}
-	// sorting 1st part
-	{
-		vector<int>cnt(n),pos(n);
-		for(int i=0;i<n;++i)
-		{
-			cnt[a[i].first.first]++;
-		}
+      vector<int>new_pn(n);
+      for(int i=0;i<n;++i)
+      {
+           int p=pos[c[pn[i]]];
+           new_pn[p]=pn[i];
+           pos[c[pn[i]]]++;
+      }
+      pn=new_pn;
 
-		pos[0]=0;
-		for(int i=1;i<n;++i)
-		{
-			pos[i]=pos[i-1]+cnt[i-1];
-		}
-		vector<pair<pair<int,int>,int> >new_a(n);
-		for(int i=0;i<n;++i)
-		{
-			int p=a[i].first.first;
-			new_a[pos[p]]=a[i];
-			pos[p]++;
-		}
-		a=new_a;
-	}
-	return;
-}
+      return;
+
+} /// end counting sort
 void HalfDead()
 {
-	string s;
-	cin>>s;
-	s+="$";
+      string s;
+      cin>>s;
+      s+="$";
+      n=(int)s.size();
+      
+      // cyclick shift when k=0
+      vector<pair<char,int> >a(n);
+      for(int i=0;i<n;++i)
+      {
+            a[i]={s[i],i};
+      }
+      sort(all(a));
+      vector<int>pn(n),c(n);
+      for(int i=0;i<n;++i)
+      {
+            pn[i]=a[i].second;
+      }
+      c[pn[0]]=0;
+      for(int i=1;i<n;++i)
+      {
+            if(a[i].first==a[i-1].first)c[pn[i]]=c[pn[i-1]];
+            else c[pn[i]]=c[pn[i-1]]+1;
+      }
+      // while 2^k <n then cyclic shift
+      int k=0;
+      while((1<<k)<n)
+      {
+            for(int i=0;i<n;++i)
+            {
+                  pn[i]=(pn[i]-(1<<k)+n)%n;
+            }
+            counting_sort(pn,c);
+            //  assigning new class
+            vector<int>new_c(n);
+            new_c[pn[0]]=0;
+            for(int i=1;i<n;++i)
+            {
+                  pair<int,int>now={c[pn[i]],c[(pn[i]+(1<<k))%n]};
+                  pair<int,int>prev={c[pn[i-1]],c[(pn[i-1]+(1<<k))%n]};
+                  if(now==prev)new_c[pn[i]]=new_c[pn[i-1]];
+                  else new_c[pn[i]]=new_c[pn[i-1]]+1;
+            }
+            c=new_c;
+            ++k;
+      }
 
-	int n=s.size();
-	int k=0;
-	vector<int>p(n),c(n); // p for position of sorted suffix array ,c for equivlent classs
-
-	// calculation for k=0
-	vector<pair<char,int> >a(n);
-	for(int i=0;i<n;++i)a[i]={s[i],i};
-	sort(a.begin(),a.end());
-
-	for(int i=0;i<n;++i)p[i]=a[i].second;
-	c[p[0]]=0;
-	for(int i=1;i<n;++i)
-	{
-		if(a[i].first==a[i-1].first)c[p[i]]=c[p[i-1]];
-		else c[p[i]]=c[p[i-1]]+1;
-	}
-
-	while((1<<k)<n)
-	{
-		vector<pair<pair<int,int> ,int> >pr(n);
-		for(int i=0;i<n;++i)
-		{
-			pr[i]=make_pair(make_pair(c[i],c[(i+(1<<k))%n]),i); // making pair from class
-		}
-		radix_sort(pr);
-
-		for(int i=0;i<n;++i)p[i]=pr[i].second;  // assigning position
-		c[p[0]]=0;
-		
-		for(int i=1;i<n;++i)
-		{
-			if(pr[i].first==pr[i-1].first)
-			{
-				c[p[i]]=c[p[i-1]];
-			}
-			else
-			{
-				c[p[i]]=c[p[i-1]]+1;
-			}
-		}
-		k++;
-	}
-
-	for(int i=0;i<n;++i)cout<<p[i]<<" "; cout<<endl;
+      for(int i=0;i<n;++i)cout<<pn[i]<<" "; cout<<endl;
 }
 int main()
 {
-	ios_base::sync_with_stdio(0); cin.tie(nullptr);
-	HalfDead();
-
-	return 0;
+      ios_base::sync_with_stdio(0); cin.tie(nullptr);
+      int tc=1;
+     // cin>>tc;
+      while(tc--)
+      HalfDead();
+      return 0;
 }
