920e7de3ce23e0bf53a116fddd12e5c6a578e596
Fuad Ashraful Mehmet <43589290+fuadashraful@users.noreply.github.com>
2024-09-25 20:51:45 +0600

    Refactor trie with a cleaner way solving https://leetcode.com/problems/sum-of-prefix-scores-of-strings

--- trie.cpp
+++ trie.cpp
@@ -1,83 +1,66 @@
-/*
-Hey  why peeping here -_'_- ?
-I believe on myself and I will achieve
-this->author = Fuad Ashraful Mehmet, CSE ,University of Asia Pacific
-Todo:
-uva : 11488 - Hyper Prefix Sets
-algo : Trie
-*/
+//https://leetcode.com/problems/sum-of-prefix-scores-of-strings/
+struct Trie {
+    Trie *next[26];
+    int cnt;
 
-#include<bits/stdc++.h>
-#define forn(i,n) for(int i=0;i<(int)n;++i)
-#define all(o) (o).begin(),(o).end()
-#define sz(o) (o).size() 
-#define dbg(o)  cerr<<"at "<<__LINE__<<" response = "<<o<<endl;
-#define dbgV(O) cerr << #O << "=["; for(auto o : O) cerr << o << ", "; cerr << "E]\n";
-#define pb push_back
-using namespace std;
-using pii=pair<int,int>;
-typedef long long ll;
-int n,m,tc,q,k,cs=1,len;
-const int N=10005;
-int ans=0;
+    Trie(){
+        cnt = 0;
+        for(int i=0;i<26;++i){
+            next[i] = nullptr;
+        }
+    }
+};
 
-struct node
-{
-	bool flag;
-	int cnt;
-	node* next[2];
-	node()
-	{
-		flag=false;
-		cnt=0;
-		next[0]=next[1]=nullptr;
-	}
-}*head;
+class Solution {
+public:
 
-void insert_str(string &s)
-{
-	node* now=head;
-	
-	len=(int)sz(s);
-	forn(i,len)
-	{
-		int idx=s[i]-'0';
-		if(now->next[idx]==nullptr)
-		{
-			now->next[idx]=new node();
-		}
-		now=now->next[idx];
-		now->cnt++;
+    Trie* head = new Trie();
+    
+    void insert(string &s){
+        int n = s.size();
 
-		ans=max(ans,now->cnt*(i+1));
-	}
-	now->flag=true;
-}
-void HalfDead()
-{	
-	head=nullptr;
-	head=new node();
-	cin>>n;
-	string s;
-	forn(i,n)
-	{
-		cin>>s;
-		insert_str(s);
-	}
+        Trie* now = head;
 
-	cout<<ans<<"\n";
-	ans=1;
-	delete(head);
-}
+        for(char ch:s) {
+            int idx = ch - 'a';
 
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(nullptr);  cout.tie(nullptr);
-    //cout<<setprecision(10)<<fixed;
-	int tc=1;
-	cin>>tc;
-	while(tc--){
-		HalfDead();
-	}
- 	return 0;
-}
+            if(!now->next[idx]) {
+                now->next[idx] = new Trie();
+            }
+
+            now = now->next[idx];
+            now->cnt++;
+        }
+
+        return;
+    }
+
+    int getScore(string& s) {
+        Trie* now = head;
+
+        int sum = 0;
+
+        for(char ch: s) {
+            int idx =ch - 'a';
+            now=now->next[idx];
+            sum+=now->cnt;
+        }
+
+        return sum;
+    }
+
+    vector<int> sumPrefixScores(vector<string>& words) {
+        for(string s: words) {
+            insert(s);
+        }
+
+        vector<int>res;
+
+        for(string s: words) {
+            int r = getScore(s);
+            res.push_back(r);
+        }
+
+        return res;
+    }
+};
