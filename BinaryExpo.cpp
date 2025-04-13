3562528c27393e3fb5f8d32ab23e3f0524cd6343
Fuad Ashraful Mehmet <43589290+fuadashraful@users.noreply.github.com>
2025-04-13 23:22:37 +0600

    Update BinaryExpo.cpp to make existing code cleaner

--- BinaryExpo.cpp
+++ BinaryExpo.cpp
@@ -1,68 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long int
 #define pb push_back
-#define _FasterIO  ios_base::sync_with_stdio(false);  cin.tie(0); cout.tie(0);
-/*
- Author: fuad Ashraful Mehmet
- University of Asia Pacific
- created :16 Aug 2019 10.58 am
- Category :Number thoery (BigMod Problem)
- Problem: uva 1230
-*/
+#define _FasterIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
 using namespace std;
-//int row[]= {-1,0,1,0};
-//int col[]= {0,-1,0,1};
 
-//int row [] = {+1,-1,+0,+0,-1,-1,+1,+1}; ///fuck array for eight direction
-//int col [] = {+0,+0,+1,-1,+1,-1,-1,+1}; /// fuck array for eight direction
-int tc,cs=1,n;
-const int N=150005;
+int tc;
+const int N = 150005;
 
-ll BinaryExpo(ll x,ll y,ll n) /// this function  return x^y mod n
-{
-
-    ll res=1LL;
-  /// here n is mod number
-
-    while(y!=0)
-    {
-
-        if(y&1)
-        {
-            res=(res*x)%n;
+// Function to compute (x^y) % n using binary exponentiation
+ll binaryExpo(ll x, ll y, ll n) {
+    ll res = 1LL;
+    while (y != 0) {
+        if (y & 1) {
+            res = (res * x) % n;
         }
-
-        x=(x*x)%n;
-
-        y>>=1;
+        x = (x * x) % n;
+        y >>= 1;
     }
+    return res;
+}
 
-   return res;
+void solve() {
+    ll x, y, n;
+    cin >> x >> y >> n;
+    cout << binaryExpo(x, y, n) << '\n';
+}
 
-} /// ... end func
-
-void Solve()
-{
-
-   ll x,y,n;
-   cin>>x>>y>>n;
-   cout<<BinaryExpo(x,y,n)<<endl;
-
-} /// .. end func
-int main()
-{
-
-    _FasterIO
-
-      // freopen("input.txt","r",stdin);
-    //  freopen("output.txt","w",stdout);
-
-   cin>>tc;
-   while(tc--)
-    Solve();
-
-    bool x;
-    cin>>x;
-
+int main() {
+    _FasterIO;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    bool dummy;
+    cin >> dummy;
     return 0;
 }
